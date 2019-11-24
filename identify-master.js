de8d61cb6c58648fd8d690e2efed5434d8200a97
Stephen McGruer <smcgruer@chromium.org>
2019-11-24 16:45:54 -0500

    Add script for identifying Jan-June 2018 master runs that are missing tags

---
+++ identify-master.js
@@ -0,0 +1,115 @@
+'use strict';
+
+/**
+ * Attempts to identify untagged master runs (from a list supplied by audit.js),
+ * by examining how many files the run touched.
+ */
+
+const fs = require('fs');
+const Git = require('nodegit');
+const moment = require('moment');
+
+async function getMergePRShas() {
+  let data = await fs.promises.readFile('merge_pr_shas.txt', 'utf-8');
+  return new Set(data.split('\n'));
+}
+
+async function main() {
+  // bare clone of https://github.com/foolip/wpt-results
+  const repo = await Git.Repository.init('wpt-results.git', 1);
+
+  let merge_pr_shas = await getMergePRShas();
+
+  let data = await fs.promises.readFile('tagless-runs.txt', 'utf-8');
+  let lines = data.split('\n');
+
+  let runs = [];
+  let before = moment();
+  for (const line of lines) {
+    if (!line) continue;
+
+    // Entries look like:
+    // edge,79.0.309.7,2019-10-31T22:26:51.99Z,2019-11-01T00:34:26.568Z,f35d1c48af,run/321670007/results
+    const parts = line.split(',');
+    if (parts.length != 6) {
+      throw new Error(`Unable to parse line: ${line}`);
+    }
+
+    let run = {
+      browserName: parts[0],
+      browserVersion: parts[1],
+      startTime: parts[2],
+      endTime: parts[3],
+      sha: parts[4],
+      tag: parts[5],
+    };
+
+    // Filter for only 2018.
+    if (!run.startTime.startsWith('2018')) {
+      continue;
+    }
+
+    let commit = await repo.getReferenceCommit(run.tag);
+    let diffList = await commit.getDiff();
+    if (diffList.length != 1) {
+      throw new Error(`${run.tag} has diffList of length ${diffList.length} (expected 1)`);
+    }
+
+    // This is the slow bit. Takes about a second per run.
+    let stats = await diffList[0].getStats();
+    run.insertions = stats.insertions();
+
+    runs.push(run);
+  }
+
+  // Now sort them by insertions (descending).
+  runs.sort((runA, runB) => {
+    // If runB has more insertions, this is positive and so will sort runB to
+    // come before runA.
+    return runB.insertions - runA.insertions;
+  });
+
+  // During 2018, the number of tests grows from about 24k in January to 27.5k
+  // in June. We use that to map to our expectations for tests.
+  const expected_insertions = new Map([
+      [ '2018-01', 24000 ],
+      [ '2018-02', 25000 ],
+      [ '2018-03', 25000 ],
+      [ '2018-04', 25500 ],
+      [ '2018-05', 26000 ],
+      [ '2018-06', 27500 ],
+  ]);
+
+  let run_ids = [];
+  for (const run of runs) {
+    const in_merge_pr_shas = merge_pr_shas.has(run.sha);
+    console.log(`${run.tag} (${run.browserName} ${run.browserVersion}, ${run.startTime.substring(0, 10)}, ${run.sha}) has ${run.insertions} insertions` +
+        (!in_merge_pr_shas ? ' <-- NOT IN MERGE PR SHAS' : ''));
+
+    // Filter for in merge_pr_shas.
+    if (!in_merge_pr_shas) {
+      continue;
+    }
+
+    const month = run.startTime.substring(0, 7);
+    if (!expected_insertions.has(month)) {
+      continue;
+    }
+
+    if (run.insertions >= expected_insertions.get(month)) {
+      run_ids.push(run.tag.split('/')[1]);
+    }
+  }
+
+  let after = moment();
+  console.log(`Processed ${lines.length} runs in ${after - before}ms`);
+
+  let output = 'should_be_master_run_ids_2018.txt';
+  console.log(`Writing to ${output}`);
+  await fs.promises.writeFile(output, run_ids.join('\n'), 'utf-8');
+}
+
+main().catch(reason => {
+  console.error(reason);
+  process.exit(1);
+});
